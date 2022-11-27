f76e1605d11c6323db57e2bd4dcfb9b4e83c3375
nonso <nonsoanetoh@gmail.com>
2022-11-27 03:10:39 +0300

    added deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,6 +6,7 @@ require("./tasks/block-number.task");
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const WALLET_PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const LOCAL_HARDHAT_NODE_URL = process.env.LOCAL_HARDHAT_NODE_URL;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,6 +16,12 @@ module.exports = {
       url: GOERLI_RPC_URL,
       accounts: [WALLET_PRIVATE_KEY],
       chainId: 5,
+    },
+    localhost: {
+      url: LOCAL_HARDHAT_NODE_URL,
+      // no need fo accounts. hardhat generates 19 for us to use
+      // accounts: [],
+      chainId: 31337,
     },
   },
   etherscan: {
